40cb02383438ad1eedcfbad1a4e8b5a32596c37a
w <no>
2024-09-18 17:30:38 -0400

    new

--- intervention/task.py
+++ intervention/task.py
@@ -108,8 +108,6 @@ def generate_and_save_acts(
                         best_logit = float("-inf")
                         best_token = None
                         for token in task.allowable_tokens:
-                            print('model', model)
-                            import pdb; pdb.set_trace()
                             logit = last_logits[model.to_single_token(token)]
                             if logit > best_logit:
                                 best_logit = logit
